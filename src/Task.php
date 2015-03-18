3ba3e9678ef177b9b3fef4343675403e32f62776
Tom Mertz <tom.mertz@verizon.net>
2015-03-17 21:54:12 -0700

    Update class to include category id

--- src/Task.php
+++ src/Task.php
@@ -1,77 +1,84 @@
 <?php
-
-class Task
-{
-    private $description;
-    private $id;
-
-    function __construct($description, $id = null)
+    class Task
     {
-        $this->description = $description;
-        $this->id = $id;
-    }
+        private $description;
+        private $category_id;
+        private $id;
 
-    function getDescription()
-    {
-        return $this->description;
-    }
-
-    function setDescription($new_desc)
-    {
-        $this->description = (string) $new_desc;
-    }
-
-    function getId()
-    {
-        return $this->id;
-    }
-
-    function setId($new_id)
-    {
-        $this->id = (int) $new_id;
-    }
-
-    function save()
-    {
-        $statement = $GLOBALS['DB']->query("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}') RETURNING id;");
-        $result = $statement->fetch(PDO::FETCH_ASSOC);
-        $this->setId($result['id']);
-    }
-
-    static function getAll()
-    {
-        $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
-        $tasks = array();
-
-        foreach ($returned_tasks as $task) {
-            $description = $task['description'];
-            $id = $task['id'];
-            $new_task = new Task($description, $id);
-            array_push($tasks, $new_task);
-        }
-
-        return $tasks;
-    }
-
-    static function deleteAll()
-    {
-        $GLOBALS['DB']->exec("DELETE FROM tasks *;");
-    }
-
-    static function find($search_id)
-    {
-        $found_task = null;
-        $tasks = Task::getAll();
-
-        foreach($tasks as $task)
+        function __construct($description, $id = null, $category_id)
         {
-            $task_id = $task->getId();
-            if($task_id == $search_id) {
-                $found_task = $task;
-            }
+            $this->description = $description;
+            $this->id = $id;
+            $this->category_id = $category_id;
         }
-        return $found_task;
-    }
-}
 
+        function setDescription($new_description)
+        {
+            $this->description = (string) $new_description;
+        }
+
+        function getDescription()
+        {
+            return $this->description;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function setId($new_id)
+        {
+            $this->id = (int) $new_id;
+        }
+
+        function setCategoryId($new_category_id)
+        {
+            $this->category_id = (int) $new_category_id;
+        }
+
+        function getCategoryId()
+        {
+            return $this->category_id;
+        }
+
+        function save()
+        {
+            $statement = $GLOBALS['DB']->query("INSERT INTO tasks (description, category_id) VALUES ('{$this->getDescription()}', {$this->getCategoryId()}) RETURNING id;");
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+            $this->setId($result['id']);
+        }
+
+        static function getAll()
+        {
+            $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
+            $tasks = array();
+            foreach($returned_tasks as $task) {
+                $description = $task['description'];
+                $id = $task['id'];
+                $category_id = $task['category_id'];
+                $new_task = new Task($description, $id, $category_id);
+                array_push($tasks, $new_task);
+            }
+            return $tasks;
+        }
+
+        static function deleteAll()
+        {
+          $GLOBALS['DB']->exec("DELETE FROM tasks *;");
+        }
+
+        static function find($search_id)
+        {
+            $found_task = null;
+            $tasks = Task::getAll();
+            foreach($tasks as $task) {
+                $task_id = $task->getId();
+                if ($task_id == $search_id) {
+                  $found_task = $task;
+                }
+            }
+            return $found_task;
+        }
+    }
 ?>
