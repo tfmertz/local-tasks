008da0865e7b0233bf7448dc01143eff0a402192
Tom Mertz <tom.mertz@verizon.net>
2015-03-17 21:54:51 -0700

    Add getTasks test

--- tests/CategoryTest.php
+++ tests/CategoryTest.php
@@ -134,6 +134,31 @@
             //Assert
             $this->assertEquals($test_Category, $result);
         }
+
+        function test_getTasks()
+        {
+            //Arrange
+            $name = "Work stuff";
+            $id = null;
+            $test_category = new Category($name, $id);
+            $test_category->save();
+
+            $test_category_id = $test_category->getId();
+
+            $description = "Email client";
+            $test_task = new Task($description, $id, $test_category_id);
+            $test_task->save();
+
+            $description2 = "Meet with boss";
+            $test_task2 = new Task($description2, $id, $test_category_id);
+            $test_task2->save();
+
+            //Act
+            $result = $test_category->getTasks();
+
+            //Assert
+            $this->assertEquals([$test_task, $test_task2], $result);
+        }
     }
 
 ?>
