854f85ee73fcf0cc0bf96ed2b31f10673613222b
Tom Mertz <tom.mertz@verizon.net>
2015-03-22 18:10:27 -0700

    Add basic html

--- app/app.php
+++ app/app.php
@@ -1,6 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Category.php";
     require_once __DIR__."/../setup.config";
 
     $app = new Silex\Application();
@@ -12,19 +13,7 @@
     $app->get("/", function() use ($app) {
         $allTasks = Task::getAll();
 
-        return $app['twig']->render('tasks.php', array('tasks' => Task::getAll()));
-    });
-
-    $app->post("/tasks", function() use ($app) {
-        $task = new Task($_POST['description']);
-        $task->save();
-
-        return $app['twig']->render('tasksCreated.php', array('task' => $task));
-    });
-
-    $app->post("/deleteTasks", function() use ($app){
-        Task::deleteAll();
-        return $app['twig']->render('deleteTasks.php');
+        return $app['twig']->render('homepage.twig');
     });
 
     return $app;
