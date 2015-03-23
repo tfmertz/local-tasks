3f4c1bb2329e70980fb6be10b0c5a609c0321549
Tom Mertz <tom.mertz@verizon.net>
2015-03-22 18:32:22 -0700

    Add categories/tasks routes

--- app/app.php
+++ app/app.php
@@ -5,15 +5,25 @@
     require_once __DIR__."/../setup.config";
 
     $app = new Silex\Application();
+    $app['debug'] = true;
 
     $DB = new PDO('pgsql:host=localhost;dbname=to_do', $DB_USER, $DB_PASS);
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
     $app->get("/", function() use ($app) {
-        $allTasks = Task::getAll();
 
         return $app['twig']->render('homepage.twig');
+    });
+
+    $app->get("/categories", function() use ($app) {
+
+        return $app['twig']->render('categories.twig', array('category_array' => Category::getAll()));
+    });
+
+    $app->get("/tasks", function() use ($app) {
+
+        return $app['twig']->render('tasks.twig', array('task_array' => Task::getAll()));
     });
 
     return $app;
